b54cc6ac0176dac5d0c0d9e8c668f3fdeac6c8e5
margaritasing <libiamfreitesm@gmail.com>
2022-05-14 18:38:15 -0300

    agrego el archivo de la db

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 
 //Definimos ruta principal
+app.get('/', (req, res)=>{
+    res.send('que fue')
+})
 
 app.listen(4000, ()=> {
     console.log("Servidor corriendo en el 4000")
