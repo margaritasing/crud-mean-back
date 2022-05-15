02555d85c5570ebcbba8464a4d3fae4b6708b468
margaritasing <libiamfreitesm@gmail.com>
2022-05-15 17:59:56 -0300

    agrego el cors

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const conectarDb = require('./config/db')
+const cors = require("cors")
 
-const app = express()
+const app = express();
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 
 
 //conectamos a la db
-conectarDb()
+conectarDb();
+app.use(cors());
 
 //rutas
-app.use('/api/productos', require('./routes/producto'))
+app.use('/api/productos', require('./routes/producto'));
 
 
 
 app.listen(4000, ()=> {
-    console.log("Servidor corriendo en el 4000")
+    console.log("Servidor corriendo en el 4000");
 })
